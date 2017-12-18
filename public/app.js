7a57ac73801f2de6c3e5e6e027944b1433c1b76a
kconnelly86 <kconnelly86@yahoo.com>
2017-12-18 09:15:41 -0600

    beautified files

--- public/app.js
+++ public/app.js
@@ -1,160 +1,137 @@
-/* Translator 
- * front-end
- * ==================== */
- var searchQuery;
- var queryAPI;
- var lang;
- var theTranslation;
- var langFull;
-
+/* Translator
+* front-end
+* ==================== */
+var searchQuery;
+var queryAPI;
+var lang;
+var theTranslation;
+var langFull;
 // Loads results onto the page
 function getResults() {
-  // Empty any results currently on the page
-  $("#results").empty();
-  // Grab all of the current translations
-  $.getJSON("/all", function(data) { console.log(data);
-    // For each translation...
-    for (var i = 0; i < data.length; i++) {
-      // ...populate #results with a p-tag that includes the translation's translate and object id
-      $("#results").prepend("<div id='cont1' data-id=" + data[i]._id + ">" + "<p class='dataentry' data-id=" + data[i]._id + "><span class='datatranslate' data-id=" + data[i]._id + ">" + data[i].translate + "</span> <span class=deleter>X</span> <span class='datatranslation' data-id=" +
-        data[i]._id + ">" + data[i].translation + "</span> <span class='datatranslation' data-id=" +
-        data[i]._id + ">" + data[i].langFull + "</span> </p>" + "</div>");
-      console.log(data[i]._id);
-    }
-  });
+// Empty any results currently on the page
+$("#results").empty();
+// Grab all of the current translations
+$.getJSON("/all", function(data) { console.log(data);
+// For each translation...
+for (var i = 0; i < data.length; i++) {
+// ...populate #results with a p-tag that includes the translation's translate and object id
+$("#results").prepend("<div id='cont1' data-id=" + data[i]._id + ">" + "<p class='dataentry' data-id=" + data[i]._id + "><span class='datatranslate' data-id=" + data[i]._id + ">" + data[i].translate + "</span> <span class=deleter>X</span> <span class='datatranslation' data-id=" +
+data[i]._id + ">" + data[i].translation + "</span> <span class='datatranslation' data-id=" +
+data[i]._id + ">" + data[i].langFull + "</span> </p>" + "</div>");
+console.log(data[i]._id);
 }
-
+});
+}
 // Runs the getResults function as soon as the script is executed
 getResults();
-
 // When the #makenew button is clicked
 $(document).on("click", "#makenew", function() {
-
-  // AJAX POST call to the submit route on the server
-  // This will take the data from the form and send it to the server
-    searchQuery = $("#translate").val();
-    queryAPI = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyDfyQpiTmaKJG9ri-xKSX_wnG5f2MUY6TY&target=" + lang +"&q=" + searchQuery;
-    $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: queryAPI,
-    // On a successful call, clear the #results section
-    success: function(response) {
-       console.log(response);
-      theTranslation = response.data.translations[0].translatedText;
-      console.log(response.data.translations[0].translatedText);
-      $("#translation").text(response.data.translations[0].translatedText);
-    }
+// AJAX POST call to the submit route on the server
+// This will take the data from the form and send it to the server
+searchQuery = $("#translate").val();
+queryAPI = "https://translation.googleapis.com/language/translate/v2?key=AIzaSyDfyQpiTmaKJG9ri-xKSX_wnG5f2MUY6TY&target=" + lang +"&q=" + searchQuery;
+$.ajax({
+type: "GET",
+dataType: "json",
+url: queryAPI,
+// On a successful call, clear the #results section
+success: function(response) {
+console.log(response);
+theTranslation = response.data.translations[0].translatedText;
+console.log(response.data.translations[0].translatedText);
+$("#translation").text(response.data.translations[0].translatedText);
+}
 }).done(function(response) {
-
-     $.ajax({
-       type: "POST",
-       dataType: "json",
-       url: "/submit",
-       data: {
-         translate: $("#translate").val(),
-         translation: theTranslation,
-         langFull: langFull,
-         created: Date.now()
-       }
-     })
-     // If that API call succeeds, add the translate and a delete button for the translation to the page
-     .done(function(data) {
-
-       // Add the translate and delete button to the #results section, "what was to be translated and the x".
-       $("#results").prepend("<p class='dataentry' data-id=" + data._id + "> <span class='datatranslate' data-id=" +
-         data._id + ">" + data.translate + "</span> <span class=deleter>X</span> <span class='datatranslation' data-id=" +
-         data._id + "> " + theTranslation + " </span> <span class='langFull' data-id=" +
-         data._id + "> " + langFull + " </span> </p>");
-
-       // // Clear the translation and translate inputs on the page
-       // $("#translation").val(""); //this can be commented out or in and it has no affect since other code had been implemented to make the translation stay in the window.
-       // $("#translate").val(""); //if this is commented in it will clear the translate field after submission and we dont want that.
-   }
-   );
-
+$.ajax({
+type: "POST",
+dataType: "json",
+url: "/submit",
+data: {
+translate: $("#translate").val(),
+translation: theTranslation,
+langFull: langFull,
+created: Date.now()
+}
+})
+// If that API call succeeds, add the translate and a delete button for the translation to the page
+.done(function(data) {
+// Add the translate and delete button to the #results section, "what was to be translated and the x".
+$("#results").prepend("<p class='dataentry' data-id=" + data._id + "> <span class='datatranslate' data-id=" +
+data._id + ">" + data.translate + "</span> <span class=deleter>X</span> <span class='datatranslation' data-id=" +
+data._id + "> " + theTranslation + " </span> <span class='langFull' data-id=" +
+data._id + "> " + langFull + " </span> </p>");
+// // Clear the translation and translate inputs on the page
+// $("#translation").val(""); //this can be commented out or in and it has no affect since other code had been implemented to make the translation stay in the window.
+// $("#translate").val(""); //if this is commented in it will clear the translate field after submission and we dont want that.
+}
+);
 });
-
 });
+// dropdown menu
 
-  // dropdown menu
-  
-
-    $("select#selectLang").change(function(){
-        lang = $("#selectLang option:selected").val();
-        console.log("You have selected the country - " + lang);
-        langFull = $("#selectLang option:selected").text();
-    });
-
-
+$("select#selectLang").change(function(){
+lang = $("#selectLang option:selected").val();
+console.log("You have selected the country - " + lang);
+langFull = $("#selectLang option:selected").text();
+});
 // When the #clearall button is pressed
 $("#clearall").on("click", function() {
-
-  // Make an AJAX GET request to delete the translations from the db
-  $.ajax({
-    type: "GET",
-    dataType: "json",
-    url: "/clearall",
-    // On a successful call, clear the #results section
-    success: function(response) {
-      $("#results").empty();
-    }
-  });
+// Make an AJAX GET request to delete the translations from the db
+$.ajax({
+type: "GET",
+dataType: "json",
+url: "/clearall",
+// On a successful call, clear the #results section
+success: function(response) {
+$("#results").empty();
+}
 });
-
-
+});
 // When user clicks the deleter button for a translation
 //$('#cont1', '.datatranslate', 'datatranslation').on('click', function(){})
 $(document).on("click", ".deleter", function() {
-  // Save the p tag that encloses the button
-  var selected = $(this).parent();
-  // Make an AJAX GET request to delete the specific translation
-  // this uses the data-id of the p-tag, which is linked to the specific translation
-  $.ajax({
-    type: "GET",
-    url: "/delete/" + selected.attr("data-id"),
-
-    // On successful call
-    success: function(response) {
-      // Remove the p-tag from the DOM
-      selected.remove();
-      // Clear the translation and translate inputs
-      $("#translation").val("");
-      $("#translate").val("");
-      // Make sure the #actionbutton is submit (in case it's update)
-      $("#actionbutton").html("<button id='makenew'>Submit</button>");
-    }
-  });
+// Save the p tag that encloses the button
+var selected = $(this).parent();
+// Make an AJAX GET request to delete the specific translation
+// this uses the data-id of the p-tag, which is linked to the specific translation
+$.ajax({
+type: "GET",
+url: "/delete/" + selected.attr("data-id"),
+// On successful call
+success: function(response) {
+// Remove the p-tag from the DOM
+selected.remove();
+// Clear the translation and translate inputs
+$("#translation").val("");
+$("#translate").val("");
+// Make sure the #actionbutton is submit (in case it's update)
+$("#actionbutton").html("<button id='makenew'>Submit</button>");
+}
 });
-
+});
 // When user click's on translation, show the translation, and allow for updates
 $(document).on("click", "#cont1",  function() {
-  // Grab the element
-  var selected = $(this);
-  // Make an ajax call to find the translation
-  // This uses the data-id of the p-tag, which is linked to the specific translation
-  $.ajax({
-    type: "GET",
-    url: "/find/" + selected.attr("data-id"),
-    success: function(data) {
+// Grab the element
+var selected = $(this);
+// Make an ajax call to find the translation
+// This uses the data-id of the p-tag, which is linked to the specific translation
+$.ajax({
+type: "GET",
+url: "/find/" + selected.attr("data-id"),
+success: function(data) {
+// Fill the inputs with the data that the ajax call collected
+$("#translation").text(data.translation);
 
-      // Fill the inputs with the data that the ajax call collected
-      $("#translation").text(data.translation);
-      
-      $("#translate").val(data.translate);
-      // Make the #actionbutton an update button, so user can
-      // Update the translation s/he chooses
-      //$("#actionbutton").html("<button id='updater' data-id='" + data._id + "'>Update</button>");
-    }
-  });
+$("#translate").val(data.translate);
+// Make the #actionbutton an update button, so user can
+// Update the translation s/he chooses
+//$("#actionbutton").html("<button id='updater' data-id='" + data._id + "'>Update</button>");
+}
+});
 });
 // reseting the page without having to refresh everytime.
 function myFunction() {
-    $('#translation').empty();
-    $('#translate').val("");
-    
+$('#translation').empty();
+$('#translate').val("");
+
 }
-
-
-
-
--- server.js
+++ server.js
@@ -1,146 +1,126 @@
-/* Translator 
- * backend
- * ==================== */
-
+/* Translator
+* backend
+* ==================== */
 // Dependencies
 var express = require("express");
 var mongojs = require("mongojs");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
-
 var app = express();
-
 // Set the app up with morgan, body-parser, and a static folder
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
-  extended: false
+extended: false
 }));
 app.use(express.static("public"));
-
 // Database configuration
 var databaseUrl = "translator";
 var collections = ["translations"];
-
 // Hook mongojs config to db variable
 var db = mongojs(databaseUrl, collections);
-
 // Log any mongojs errors to console
 db.on("error", function(error) {
-  console.log("Database Error:", error);
+console.log("Database Error:", error);
 });
-
-
 // Routes
 // ======
-
 // Simple index route
 app.get("/", function(req, res) {
-  res.send(index.html);
+res.send(index.html);
 });
-
 //this section doesnt affect the translation aspect, it affects the send to the database. HAVE TO HAVE
 // Handle form submission, save submission to mongo
-app.post("/submit", function(req, res) { 
-  // Insert the translation into the translations collection
-  db.translations.insert(req.body, function(error, saved) { 
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the translation back to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      res.send(saved);
-    }
-  });
+app.post("/submit", function(req, res) {
+// Insert the translation into the translations collection
+db.translations.insert(req.body, function(error, saved) {
+// Log any errors
+if (error) {
+console.log(error);
+}
+// Otherwise, send the translation back to the browser
+// This will fire off the success function of the ajax request
+else {
+res.send(saved);
+}
 });
-
-
+});
 //if you dont have this it wont populate your results box from your database in local host. HAVE TO HAVE
 // Retrieve results from mongo
 app.get("/all", function(req, res) {
-  // Find all translations in the translations collection
-  db.translations.find({}, function(error, found) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send json of the translations back to user
-    // This will fire off the success function of the ajax request
-    else {
-      console.log("retrieving" + JSON.stringify(found));
-      res.json(found);
-      
-    }
-  });
+// Find all translations in the translations collection
+db.translations.find({}, function(error, found) {
+// Log any errors
+if (error) {
+console.log(error);
+}
+// Otherwise, send json of the translations back to user
+// This will fire off the success function of the ajax request
+else {
+console.log("retrieving" + JSON.stringify(found));
+res.json(found);
+
+}
 });
-// must have to find one/ and update the front end and populate 
+});
+// must have to find one/ and update the front end and populate
 // Select just one translation by an id
 app.get("/find/:id", function(req, res) {
-
-  // When searching by an id, the id needs to be passed in
-  // as (mongojs.ObjectId(IDYOUWANTTOFIND))
-
-  // Find just one result in the translations collection
-  db.translations.findOne({
-    // Using the id in the url
-    "_id": mongojs.ObjectId(req.params.id)
-  }, function(error, found) {
-    // log any errors
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the translation to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      res.send(found);
-    }
-  });
+// When searching by an id, the id needs to be passed in
+// as (mongojs.ObjectId(IDYOUWANTTOFIND))
+// Find just one result in the translations collection
+db.translations.findOne({
+// Using the id in the url
+"_id": mongojs.ObjectId(req.params.id)
+}, function(error, found) {
+// log any errors
+if (error) {
+console.log(error);
+res.send(error);
+}
+// Otherwise, send the translation to the browser
+// This will fire off the success function of the ajax request
+else {
+res.send(found);
+}
 });
-
-
+});
 // Delete One from the DB
 app.get("/delete/:id", function(req, res) {
-  // Remove a translation using the objectID
-  db.translations.remove({
-    "_id": mongojs.ObjectID(req.params.id)
-  }, function(error, removed) {
-    // Log any errors from mongojs
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(removed);
-      res.send(removed);
-    }
-  });
+// Remove a translation using the objectID
+db.translations.remove({
+"_id": mongojs.ObjectID(req.params.id)
+}, function(error, removed) {
+// Log any errors from mongojs
+if (error) {
+console.log(error);
+res.send(error);
+}
+// Otherwise, send the mongojs response to the browser
+// This will fire off the success function of the ajax request
+else {
+console.log(removed);
+res.send(removed);
+}
 });
-
-
+});
 // Clear the DB
 app.get("/clearall", function(req, res) {
-  // Remove every translation from the translations collection
-  db.translations.remove({}, function(error, response) {
-    // Log any errors to the console
-    if (error) {
-      console.log(error);
-      res.send(error);
-    }
-    // Otherwise, send the mongojs response to the browser
-    // This will fire off the success function of the ajax request
-    else {
-      console.log(response);
-      res.send(response);
-    }
-  });
+// Remove every translation from the translations collection
+db.translations.remove({}, function(error, response) {
+// Log any errors to the console
+if (error) {
+console.log(error);
+res.send(error);
+}
+// Otherwise, send the mongojs response to the browser
+// This will fire off the success function of the ajax request
+else {
+console.log(response);
+res.send(response);
+}
 });
-
-
+});
 // Listen on port 3000
 app.listen(3000, function() {
-  console.log("App running on port 3000!");
+console.log("App running on port 3000!");
 });
